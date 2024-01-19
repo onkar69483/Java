99a6fffde6db532cae31b89513f117edb0990b1e
battlemachine <onkar69483@gmail.com>
2024-01-19 19:41:20 +0530

    adding files

---
+++ Assignments/src/Library.java
@@ -0,0 +1,81 @@
+class Book{
+	private String book_name;
+	private int price;
+	private Author author;
+	public Book() {
+		
+	}
+	public Book(String book_name, int price, Author author) {
+		this.book_name = book_name;
+		this.price = price;
+		this.author = author;
+	}
+
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public void getDetails() {
+		System.out.println("Book Name: "+book_name);
+		System.out.println("Book Price: "+price);
+		author.getDetails();
+	}
+}
+
+class Author{
+	private String author_name;
+	private int age;
+	public Author(String author_name, int age) {
+		this.author_name = author_name;
+		this.age = age;
+	}
+	public void getDetails() {
+		System.out.println("Author Name: "+author_name);
+		System.out.println("Author Age: "+age);
+	}
+}
+
+public class Library {
+	private int book_number;
+	private Book book[];
+	public Library() {
+		this.book_number = 0;
+		this.book = new Book[5];
+	}
+	public void add_book(String book_name, int price, String author_name, int author_age) {
+		Author author = new Author(author_name, author_age);
+		if(book_number<5) {
+			this.book[this.book_number] = new Book(book_name, price, author);
+			this.book_number++;
+		}
+		else {
+			System.out.println("Library Full!");
+		}
+	}
+	
+	public void get_Books() {
+		for(int i=0; i<book_number; i++) {
+			book[i].getDetails();
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Library library = new Library();
+	    library.add_book("The Catcher in the Rye", 15, "J.D. Salinger", 35);
+	    library.add_book("To Kill a Mockingbird", 20, "Harper Lee", 34);
+	    library.add_book("1984", 18, "George Orwell", 46);
+	    library.add_book("The Great Gatsby", 25, "F. Scott Fitzgerald", 44);
+	    library.add_book("The Hobbit", 30, "J.R.R. Tolkien", 62);
+		library.get_Books();
+	}
+	
+}
--- Assignments/src/Person.java
+++ Assignments/src/Person.java
@@ -1,4 +1,54 @@
 
 public class Person {
-	
+	private String name;
+	private int age;
+	private String country;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public void display() {
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Country: "+country);
+	}
+	public static void main(String[] args) {
+		Person person[] = new Person[3];
+		person[0] = new Person();
+		person[1] = new Person();
+		person[2] = new Person();
+		
+		person[0].setName("Onkar");
+		person[0].setAge(18);
+		person[0].setCountry("India");
+		
+		person[1].setName("Sachin");
+		person[1].setAge(25);
+		person[1].setCountry("US");
+		
+		person[2].setName("Mihir");
+		person[2].setAge(26);
+		person[2].setCountry("Africa");
+		
+		System.out.println("Displaying Person Details");
+		for(int i=0; i<3;i++) {
+			person[i].display();
+			System.out.println();
+		}
+		
+	}
 }

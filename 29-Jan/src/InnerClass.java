d181ea29f78b3c68fe6bd2d3386fef1dc1358e1a
battlemachine <onkar69483@gmail.com>
2024-01-29 09:53:48 +0530

    adding files

--- 29-Jan/src/InnerClass.java
+++ 29-Jan/src/InnerClass.java
@@ -1,4 +1,38 @@
 
 public class InnerClass {
+	private int oclassv = 10;
+	
+	public int getOclassv() {
+		return oclassv;
+	}
 
+	class Iclass{
+		int iclassv;
+		public void dispOvar() {
+			System.out.println("Val of Outer Class var: "+oclassv);
+		}
+	}
+	
+	public static void main(String[] args) {
+		InnerClass c1 = new InnerClass();
+		System.out.println(c1.getOclassv());
+		
+		InnerClass.Iclass i1 = c1.new Iclass();
+		i1.dispOvar();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

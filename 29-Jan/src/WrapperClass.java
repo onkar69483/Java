cf405e0316a56ae050a403a083980a8e7f9ddc69
battlemachine <onkar69483@gmail.com>
2024-01-29 09:19:10 +0530

    adding files

---
+++ 29-Jan/src/WrapperClass.java
@@ -0,0 +1,21 @@
+//Used to convert any primitive data type to object
+//u get wrapper classes for specific data type eg - int has wrapper class Integer and char has wrapper class Character
+// Converting the primitive data type to class implicitly is called autoboxing
+public class WrapperClass {
+	public static void main(String[] args) {
+		int a=32;
+		Integer i = Integer.valueOf(a);
+		Integer j = a; // autoboxing
+		
+		int x = j.intValue(); // not autounboxing
+		int x1 = j;  //autounboxing
+		
+		
+		float f = j.floatValue(); // Converting to float explicitly
+		float f1 = j; //Compiler automatically converts to float implicitly also known as autounboxing 
+		
+		System.out.println(j.compareTo(i));
+		
+		System.out.println("i: "+i+"\nj: "+j+"\nx: "+x+"\nx1: "+x1+"\nf: "+f+"\nf1: "+f1);
+	}
+}

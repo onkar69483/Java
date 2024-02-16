49d0e3da9e68fc5ef24985091b5ef2461398ed29
battlemachine <onkar69483@gmail.com>
2024-02-16 10:38:46 +0530

    adding unit 4 - connecting database

--- Feb-16/src/LoginDemo.java
+++ Feb-16/src/LoginDemo.java
@@ -1,4 +1,26 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
 public class LoginDemo {
-	
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Class named class in java
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/flexi","Onkar", "Prem@1234");
+			System.out.println("Connection Established");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from login");
+			ResultSetMetaData rsMetaData = rs.getMetaData();
+			System.out.println(rsMetaData.getColumnName(1) + "	" + rsMetaData.getColumnName(2));
+			while(rs.next()) {
+				System.out.println(rs.getString(1) + "	" + rs.getString(2));
+				
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }

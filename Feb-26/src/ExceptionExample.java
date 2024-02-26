7991cc5873a1eb214e72fc6e62625047ad374679
battlemachine <onkar69483@gmail.com>
2024-02-26 09:46:30 +0530

    updating notes

---
+++ Feb-26/src/ExceptionExample.java
@@ -0,0 +1,28 @@
+
+public class ExceptionExample {
+	public static void main(String[] args) {
+		int []array = new int[2];
+		array[0] = 1;
+		array[1] = 2;
+		try {
+//			System.out.println(array[3]);
+			int i=50;
+			int c = i/0;
+		} 
+		catch(ArithmeticException e) {
+			System.out.println("ArithmeticException occured");
+			
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			System.out.println("ArrayIndexOutOfBoundsException Occured");
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Exception occured");
+		}
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+
+	}
+}
